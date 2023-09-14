8845b9c6f91b6f788ac6787d1660549784acd21f
Ilija Dončić <75073537+Ika1009@users.noreply.github.com>
2023-09-14 11:10:37 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,36 +1,43 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    
-    if (request.action === "startMessaging") {
-        checkOrInjectContentScript(sender.tab.id, "contentMessaging.js", { count: request.count }, function(data) {
-            // After ensuring the script is there, send the command.
-            chrome.tabs.sendMessage(sender.tab.id, {
-                action: "initiateMessaging",
-                count: data.count
+    // Fetch the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        const activeTab = tabs[0];
+        
+        const tabId = activeTab.id;
+
+        if (request.action === "startMessaging") {
+            checkOrInjectContentScript(tabId, "contentMessaging.js", "messagingScript", { count: request.count }, function(data) {
+                chrome.tabs.sendMessage(tabId, {
+                    action: "initiateMessaging",
+                    count: data.count
+                });
             });
-        });
-    } 
-    else if (request.action === "startSwiping") {
-        checkOrInjectContentScript(sender.tab.id, "contentSwiping.js", { time: request.time, percentage: request.percentage }, function(data) {
-            // After ensuring the script is there, send the command.
-            chrome.tabs.sendMessage(sender.tab.id, {
-                action: "initiateSwiping",
-                time: data.time,
-                percentage: data.percentage
+        } 
+        else if (request.action === "startSwiping") {
+            checkOrInjectContentScript(tabId, "contentSwiping.js", "swipingScript", { time: request.time, percentage: request.percentage }, function(data) {
+                chrome.tabs.sendMessage(tabId, {
+                    action: "initiateSwiping",
+                    time: data.time,
+                    percentage: data.percentage
+                });
             });
-        });
-    }
+        }
+    });
 });
 
-function checkOrInjectContentScript(tabId, scriptFile, data, callback) {
-    chrome.tabs.sendMessage(tabId, { action: "checkScriptPresence" }, function(response) {
-        
-        // If an error is thrown or the script is not present, inject it.
+function checkOrInjectContentScript(tabId, scriptFile, scriptName, data, callback) {
+    chrome.tabs.sendMessage(tabId, { action: "checkScriptPresence", scriptName: scriptName }, function(response) {
         if (chrome.runtime.lastError || !response || !response.scriptPresent) {
-            chrome.tabs.executeScript(tabId, { file: scriptFile }, function() {
-                if (chrome.runtime.lastError) {
-                    console.error(chrome.runtime.lastError.message);
-                } else {
-                    callback(data);
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                files: [scriptFile]
+            }, (injectionResults) => {
+                for (const frameResult of injectionResults) {
+                    if (chrome.runtime.lastError) {
+                        console.log(chrome.runtime.lastError.message);
+                    } else {
+                        callback(data);
+                    }
                 }
             });
         } else {
@@ -38,4 +45,3 @@ function checkOrInjectContentScript(tabId, scriptFile, data, callback) {
         }
     });
 }
-
