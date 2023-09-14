fcf935b5ed53d288363eafc9ed0b34a6c0682d44
Ilija Dončić <75073537+Ika1009@users.noreply.github.com>
2023-09-14 10:00:06 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,41 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    
+    if (request.action === "startMessaging") {
+        checkOrInjectContentScript(sender.tab.id, "contentMessaging.js", { count: request.count }, function(data) {
+            // After ensuring the script is there, send the command.
+            chrome.tabs.sendMessage(sender.tab.id, {
+                action: "initiateMessaging",
+                count: data.count
+            });
+        });
+    } 
+    else if (request.action === "startSwiping") {
+        checkOrInjectContentScript(sender.tab.id, "contentSwiping.js", { time: request.time, percentage: request.percentage }, function(data) {
+            // After ensuring the script is there, send the command.
+            chrome.tabs.sendMessage(sender.tab.id, {
+                action: "initiateSwiping",
+                time: data.time,
+                percentage: data.percentage
+            });
+        });
+    }
+});
+
+function checkOrInjectContentScript(tabId, scriptFile, data, callback) {
+    chrome.tabs.sendMessage(tabId, { action: "checkScriptPresence" }, function(response) {
+        
+        // If an error is thrown or the script is not present, inject it.
+        if (chrome.runtime.lastError || !response || !response.scriptPresent) {
+            chrome.tabs.executeScript(tabId, { file: scriptFile }, function() {
+                if (chrome.runtime.lastError) {
+                    console.error(chrome.runtime.lastError.message);
+                } else {
+                    callback(data);
+                }
+            });
+        } else {
+            callback(data);
+        }
+    });
+}
 
